9b0ebc9095267229ba4a278d22f44e97ef725de9
Pim van den Berg <pim.van.den.berg@mendix.com>
2013-10-31 13:55:14 +0100

    debian/dirvish.cron.d: log stderr + stdout to nightly.log

--- debian/dirvish.cron.d
+++ debian/dirvish.cron.d
@@ -1,4 +1,4 @@
 # /etc/cron.d/dirvish: crontab fragment for dirvish
 
 # run every night
-4 22 * * *     root	/etc/dirvish/dirvish-cronjob 2>&1 >> /var/log/dirvish/nightly.log
+4 22 * * *     root	/etc/dirvish/dirvish-cronjob >> /var/log/dirvish/nightly.log 2>&1
